592763b6c6442b6d47626ea91c948cdcf7ff3479
zaeem571 <zaeem.m571@gmail.com>
2025-11-04 23:24:10 +0500

    corrected path for favicon

--- app/layout.js
+++ app/layout.js
@@ -64,11 +64,28 @@ export const metadata = {
   authors: [{ name: siteName }],
   creator: siteName,
   publisher: siteName,
-  //viewport: {
-  //width: "device-width",
-  //initialScale: 1,
-  //maximumScale: 5,
-  //}
+ 
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/android-chrome-512x512.png',
+      },
+    ],
+  },
+
   formatDetection: {
     email: false,
     address: false,
